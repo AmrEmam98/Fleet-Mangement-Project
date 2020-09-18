e56df2fd53ad2b1760d2d1c32cc7ad25bc5891bf
ahmedbahaa123 <ahmedbahaasayed1996@gmail.com>
2020-09-18 21:38:13 +0200

    handle Edit Details item

--- app/src/main/java/com/example/fleetmanagementsystem/carsFunctionality/activites/CarDetailsActivity.java
+++ app/src/main/java/com/example/fleetmanagementsystem/carsFunctionality/activites/CarDetailsActivity.java
@@ -1,55 +1,41 @@
 package com.example.fleetmanagementsystem.carsFunctionality.activites;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fleetmanagementsystem.R;
-import com.example.fleetmanagementsystem.carsFunctionality.adapter.DetailsAdapter;
 import com.example.fleetmanagementsystem.carsFunctionality.models.FleetModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class CarDetailsActivity extends AppCompatActivity implements DetailsAdapter.onItemClicked {
+public class CarDetailsActivity extends AppCompatActivity {
 
-
-    private DetailsAdapter detailsAdapter;
-    private RecyclerView detailsRecyclerView;
-    private List<FleetModel> carModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_details);
 
-        detailsRecyclerView= findViewById(R.id.details_recyclerView);
-        detailsAdapter = new DetailsAdapter(this);
-
-        detailsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        detailsRecyclerView.setAdapter(detailsAdapter);
-
-        carModel = new ArrayList<>();
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-        carModel.add(new FleetModel());
-
-        detailsAdapter.setList(carModel);
-
     }
 
-    @Override
-    public void onItemClicked(int position) {
-        Toast.makeText(CarDetailsActivity.this , "Edit Icon Clicked" , Toast.LENGTH_LONG).show();
+    public void editCar(View view) {
+        switch (view.getId()) {
+            case R.id.maker_Edit:
+               Toast.makeText(this , "Maker Edit" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.license_Edit:
+                Toast.makeText(this , "Lincese Edit" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.chassis_Edit:
+                Toast.makeText(this , "Chassis Edit" , Toast.LENGTH_LONG).show();
+                break;
+                // TODO Handle each edit attribute
+            default:
+                break;
+        }
     }
 }
